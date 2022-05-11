2f773593b5b4b340d74d023142b3998223e2f1f4
KateMoro <77698203+KateMoro@users.noreply.github.com>
2022-05-11 23:15:54 +0000

    Deploying to gh-pages from @ KateMoro/mishka@a98e2fd6e29b8a3bc5c5afb72d96eee98dd308b5 🚀

---
+++ js/main.js
@@ -0,0 +1,74 @@
+const mainNavButton = document.querySelector(".main-nav__toggle");
+const mainNavList = document.querySelector(".main-nav__list");
+const mainNavSearch = document.querySelector(".main-nav__search");
+const mainNavCart = document.querySelector(".main-nav__cart");
+
+const weekProductButton = document.querySelector(".week-product__button");
+const weekProductPopup = document.querySelector(".modal");
+const closePopup = document.querySelector(".modal__close");
+
+mainNavList.classList.add("main-nav__list--closed");
+mainNavSearch.classList.add("main-nav__search--closed");
+mainNavCart.classList.add("main-nav__cart--closed");
+mainNavButton.classList.remove("main-nav__toggle--no-js");
+
+mainNavButton.addEventListener("click", () => {
+  mainNavButton.classList.toggle("main-nav__toggle--opened");
+  mainNavList.classList.toggle("main-nav__list--closed");
+  mainNavSearch.classList.toggle("main-nav__search--closed");
+  mainNavCart.classList.toggle("main-nav__cart--closed");
+});
+
+weekProductButton.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  weekProductPopup.classList.add("modal--show");
+});
+
+closePopup.addEventListener("click", () => {
+  weekProductPopup.classList.remove("modal--show");
+});
+
+window.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    if (weekProductPopup.classList.contains("modal--show")) {
+      evt.preventDefault();
+      weekProductPopup.classList.remove("modal--show");
+    }
+  }
+});
+
+
+// Slider
+
+const slides = document.querySelectorAll('.reviews__item');
+const prevButton = document.querySelector('.reviews__button--prev');
+const nextButton = document.querySelector('.reviews__button--next');
+
+let sliderIndex = 1;
+
+const showSlides = (n) => {
+  if (n > slides.length) {
+    sliderIndex = 1;
+  }
+
+  if (n < 1) {
+    sliderIndex = slides.length;
+  }
+
+  slides.forEach(item => item.style.display = 'none');
+  slides[sliderIndex - 1].style.display = 'block';
+}
+
+const plusSlides = (n) => {
+  showSlides(sliderIndex += n);
+}
+
+prevButton.addEventListener('click', () => {
+  plusSlides(-1);
+});
+
+nextButton.addEventListener('click', () => {
+  plusSlides(1);
+});
+
+showSlides(sliderIndex);
